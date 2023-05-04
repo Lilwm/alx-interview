bfaea98644731df4a4e4ca36f42792a087dcd774
lilwm <lmiiri95@gmail.com>
2023-05-04 08:09:19 +0000

    pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,24 +5,21 @@
 def minOperations(n):
     """
     calculates the min number of operations to get given H's.
-    Args: 
-     function takes an integer n as input
+    Args:
+    function takes an integer n as input
 
     Returns: no of minimum operations required or 0
     """
     # Base case: if n is already equal to 1
-    if n==1:
+    if n == 1:
         return 0
-    
     # Initialize the dynamic programming array with max values
     dp = [float('inf')] * (n+1)
-    
     # For n = 1, the number of operations needed is 0
     dp[1] = 0
-    
     # Calculate minimum operations for each i from 2 to n
     for i in range(2, n+1):
-        # Try all possible factors of i
+    # Try all possible factors of i
         for j in range(1, i):
             # Check all possible factors of i and update dp[i]
             if i % j == 0:

38ba544a1a5bd9125290f897ee35926c39b452e0
lilwm <lmiiri95@gmail.com>
2023-05-04 09:17:00 +0000

    pycodestylefix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,19 +10,16 @@ def minOperations(n):
 
     Returns: no of minimum operations required or 0
     """
-    # Base case: if n is already equal to 1
-    if n == 1:
-        return 0
-    # Initialize the dynamic programming array with max values
-    dp = [float('inf')] * (n+1)
-    # For n = 1, the number of operations needed is 0
-    dp[1] = 0
-    # Calculate minimum operations for each i from 2 to n
-    for i in range(2, n+1):
-    # Try all possible factors of i
-        for j in range(1, i):
-            # Check all possible factors of i and update dp[i]
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i//j))
-    # Return the minimum operations needed to obtain n H characters
-    return dp[n] if dp[n] != float('inf') else 0
+    min_op = 1
+    start = 0
+    counter = 0
+    while min_op < n:
+        remainder = n - min_op
+        if (remainder % min_op == 0):
+            start = min_op
+            min_op += start
+            counter += 2
+        else:
+            min_op += start
+            counter += 1
+    return counter

3f51122cbc93ff0d3ffcb41213c20728e0f59efd
lilwm <lmiiri95@gmail.com>
2023-06-14 19:38:34 +0000

    initial commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!usr/bin/python3
+""" 0. Change comes from within """
+
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to meet a given amount.
+    Args:
+    coins: A list of the values of the coins in your possession.
+    total: The amount of change you need to make.
+    Returns:
+    The fewest number of coins needed to meet total.
+    If total is 0 or less, returns 0.
+    If total cannot be met by any number of coins you have, returns -1.
+    """
+    if total <= 0:
+        return 0
+    # Initialize the number of coins to 0.
+    num_coins = 0
+
+    # Iterate over the coins in descending order.
+    for coin in sorted(coins, reverse=True):
+        while coin <= total:
+            num_coins += 1
+            total -= coin
+    # If the target amount is not 0, then the number of coins needed is -1.
+    if total > 0:
+        return -1
+
+    # Return the number of coins.
+    return num_coins

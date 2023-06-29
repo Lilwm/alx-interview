10b6e6d289c30443b30de03674e63196a2cf97ff
lilwm <lmiiri95@gmail.com>
2023-06-29 14:43:47 +0000

    optimized the code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,33 +4,40 @@
 
 def isWinner(x, nums):
     """
-    gets the winner given x rounds and the array of numbers for each round.
+    Determines the winner of the game given x rounds
+    and the array of numbers for each round.
 
     Args:
-        x: The number of rounds.
-        nums: The array of numbers for each round.
+      x: The number of rounds.
+      nums: The array of numbers for each round.
 
     Returns:
-        The name of the player that won the most rounds.
-        If the winner cannot be determined, returns None.
+      The name of the player that won the most rounds.
+      If the winner cannot be determined, returns None.
     """
     maria_wins = 0
     ben_wins = 0
 
+    # Generate prime numbers up to the maximum value in nums
+    max_num = max(nums)
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_num + 1, i):
+                primes[j] = False
+
+    # Count wins for each round
     for n in nums:
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
+        prime_count = sum(primes[:n+1])
 
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-
-        if primes.count(True) % 2 == 0:
+        if prime_count % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
 
+    # Determine the winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
